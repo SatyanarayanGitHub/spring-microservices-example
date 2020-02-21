36975e15fc274ef59922082b5a3bb0fae3ea1435
Satyanarayan <satyanarayan.y@gmail.com>
2020-02-21 17:54:51 +0530

    Authentication with OAuth 2.0
    Authorization with OAuth 2.0 from okta server

--- api-gateway/src/main/java/com/srysoft/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/srysoft/example/apigateway/ApiGatewayApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+
+import com.srysoft.example.apigateway.config.UserFeignClientInterceptor;
+
+import feign.RequestInterceptor;
 
 @EnableFeignClients
 @EnableCircuitBreaker
@@ -14,6 +20,11 @@ public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RequestInterceptor getUserFeignClientInterceptor(OAuth2AuthorizedClientService clientService) {
+		return new UserFeignClientInterceptor(clientService);
 	}
 
 }
---
+++ car-service/src/main/java/com/srysoft/example/carservice/config/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.srysoft.example.carservice.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http
+			.authorizeRequests().anyRequest().authenticated()
+			.and()
+			.oauth2ResourceServer().jwt();
+		// @formatter:on
+	}
+
+}
